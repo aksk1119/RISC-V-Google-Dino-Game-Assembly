924f7592f9c72d7d51c8d1cbba59340dae112275
Seongkwan <aksk1119@gmail.com>
2022-04-12 16:12:15 -0600

    Change!

--- project.s
+++ project.s
@@ -85,6 +85,8 @@
     .eqv DINOSAUR_DUCK2_R 0x000fff0a
     .eqv DINOSAUR_JUMP_L  0x000fff0b
     .eqv DINOSAUR_JUMP_R  0x000fff0c
+    .eqv DINOSAUR_DEAD_L  0x000fff0d
+    .eqv DINOSAUR_DEAD_R  0x000fff0e
 
     .eqv DINOSAUR_RUN_ST  0x00000000
     .eqv DINOSAUR_JUMP_ST 0x00000001
@@ -98,7 +100,7 @@
     .eqv DINOSAUR_COLUMN   21
 
     .eqv JUMP_AIR_TICKS     3
-    .eqv JUMP_FALLING_TICKS 4
+    .eqv JUMP_FALLING_TICKS 5
     .eqv JUMP_GROUND_TICKS  6
 
     # Obstacles
@@ -109,6 +111,13 @@
     .eqv CHAR_BAT_U    0x000fff11
     .eqv CHAR_BAT_D    0x000fff12
     .eqv CHAR_ROCK     0x000fff01
+
+    # SCORE
+    .eqv FIRST_LOC         0x8328
+    .eqv SECOND_LOC         0x8324
+    .eqv THIRD_LOC     0x8320
+    .eqv FOURTH_LOC    0x831c
+    .eqv SCORE_CHAR_BASE        0x000fff30
 
 main:
 	# Setup the stack: sp = 0x3ffc
@@ -137,6 +146,9 @@ RESTART:
     addi t0, gp, %lo(OBSTACLE_INDEX_POINTER)
     sw x0, 0(t0)
 
+################################################################################
+# Push Buttons
+################################################################################
 PROC_BUTTONS:
 
     # Wait for a button press
@@ -148,57 +160,6 @@ PROC_BUTTONS:
     # If return is non-zero, restart
     # jal REACH_END
     j RESTART
-
-
-################################################################################
-# This procedure will check the timere and update the seven segment display
-# if the timer has reached another tick value.
-################################################################################
-UPDATE_TIMER:
-    lw t0, TIMER(tp)
-    li t1, SEGMENT_TIMER_INTERVAL
-    blt t0, t1, UT_DONE
-    # timer has reached tick, incremenet seven segmeent display and clear timer
-    sw x0, TIMER(tp)
-    lw t0, SEVENSEG_OFFSET(tp)
-    addi t0, t0, 1
-    sw t0, SEVENSEG_OFFSET(tp)
-
-LED_FOR_STATUS:
-    lw t3, %lo(DINOSAUR_STATUS)(gp)
-    li t0, DINOSAUR_JUMP_ST
-    beq t3, t0, STATUS_JUMP_LED
-    li t0, DINOSAUR_DUCK_ST
-    beq t3, t0, STATUS_DUCK_LED
-STATUS_RUN_LED:
-    li t0, 0x0001
-    sw t0, LED_OFFSET(tp)
-    j UPDATE_GRAPHIC
-STATUS_JUMP_LED:
-    li t0, 0x0002
-    sw t0, LED_OFFSET(tp)
-    j UPDATE_GRAPHIC
-STATUS_DUCK_LED:
-    li t0, 0x0004
-    sw t0, LED_OFFSET(tp)
-
-UPDATE_GRAPHIC:
-    # Update the Graphic
-    addi sp, sp, -4	    # Make room to save values on the stack
-	sw ra, 0(sp)		# Copy return address to stack
-    jal ra, DRAW_OBSTACLE
-    jal ra, DINOSAUR_CONTROL
-
-    lw ra, 0(sp)		# Restore return address
-	addi sp, sp, 4		# Update stack pointer
-
-UT_DONE:
-    jalr x0, ra, 0
-
-
-################################################################################
-# Push Buttons
-################################################################################
 
 PROCESS_BUTTONS:
     # setup stack frame and save return address
@@ -277,9 +238,58 @@ PB_CHECK_BTN_DONE:
 
     jalr x0, ra, 0
 
+################################################################################
+# TIMER AND GRAPHIC UPDATE
+#
+# This procedure will check the timere and update the seven segment display
+# and the vga graphics if the timer has reached another tick value.
+################################################################################
+UPDATE_TIMER:
+    lw t0, TIMER(tp)
+    li t1, SEGMENT_TIMER_INTERVAL
+    blt t0, t1, UT_DONE
+    # timer has reached tick, incremenet seven segmeent display and clear timer
+    sw x0, TIMER(tp)
+    lw t0, SEVENSEG_OFFSET(tp)
+    addi t0, t0, 1
+    sw t0, SEVENSEG_OFFSET(tp)
+
+LED_FOR_STATUS:
+    lw t3, %lo(DINOSAUR_STATUS)(gp)
+    li t0, DINOSAUR_JUMP_ST
+    beq t3, t0, STATUS_JUMP_LED
+    li t0, DINOSAUR_DUCK_ST
+    beq t3, t0, STATUS_DUCK_LED
+STATUS_RUN_LED:
+    li t0, 0x0001
+    sw t0, LED_OFFSET(tp)
+    j UPDATE_GRAPHIC
+STATUS_JUMP_LED:
+    li t0, 0x0002
+    sw t0, LED_OFFSET(tp)
+    j UPDATE_GRAPHIC
+STATUS_DUCK_LED:
+    li t0, 0x0004
+    sw t0, LED_OFFSET(tp)
+
+UPDATE_GRAPHIC:
+    # Update the Graphic
+    addi sp, sp, -4	    # Make room to save values on the stack
+	sw ra, 0(sp)		# Copy return address to stack
+    jal ra, DRAW_OBSTACLE
+    jal ra, DINOSAUR_CONTROL
+
+    lw ra, 0(sp)		# Restore return address
+	addi sp, sp, 4		# Update stack pointer
+
+UT_DONE:
+    jalr x0, ra, 0
 
 ################################################################################
-# The Dinasaur movement state machine
+# DINOSAUR STATE MACHINE
+#
+# State machine for the dinosaur status.
+# Also, this draws the dinosaur according to the status.
 ################################################################################
 DINOSAUR_CONTROL:
     # This chooses state based off status.
@@ -341,20 +351,37 @@ DINOSAUR_JUMP:
     # If over 6 ticks passed, go to run state.
     addi t0, x0, JUMP_GROUND_TICKS
     blt s1, t0, JUMP_STATUS_CHECK
+
+    # Set the dinosaur status to run state.
     li t0, DINOSAUR_RUN_ST
     addi t2, gp, %lo(DINOSAUR_STATUS)
     sw t0, 0(t2)
+    
+    # Draw an empty char to the prev position.
+    lw t0, %lo(EMPTY_TILE)(gp)          # Load character value to write
+    sw t0, 0(s2)                        # Draw Left side
+    addi t1, s2, 4                      
+    sw t0, 0(t1)                        # Draw Right side
+
+    # Set the dinosaur location to be the starting location (where dino runs).
+    li t0, STARTING_LOC
+    addi t1, t0, 0
+    addi t2, gp, %lo(DINOSAUR_LOC)
+    sw t1, 0(t2)
+
     j DINOSAUR_RUN
 
 JUMP_STATUS_CHECK:
     # If over 4 ticks passed, get down.
     li t0, JUMP_FALLING_TICKS
     bge s1, t0, JUMP_POS_DOWN
+
     # If over 2 ticks passed, stay up.
     li t0, JUMP_AIR_TICKS
     bge s1, t0, DRAW_JUMP
 
 JUMP_POS_UP:
+    # Increase a row for the dino's position.
     lw s2, %lo(DINOSAUR_LOC)(gp)
     addi t1, s2, NEG_ADDRESSES_PER_ROW
     addi t2, gp, %lo(DINOSAUR_LOC)
@@ -362,6 +389,7 @@ JUMP_POS_UP:
     j DRAW_JUMP
 
 JUMP_POS_DOWN:
+    # Decrease a row for the dino's position.
     lw s2, %lo(DINOSAUR_LOC)(gp)
     addi t1, s2, ADDRESSES_PER_ROW
     addi t2, gp, %lo(DINOSAUR_LOC)
@@ -427,18 +455,22 @@ DRAW_DUCK2:
     addi t1, t1, 4
     sw t0, 0(t1)
 
+    ### REMOVE THIS!
+    lw t0, %lo(ROCK_SPRITE)(gp)
+    lw t1, %lo(FIRST_VAL)(gp)
+    sw t0, 0(t1)
+    ####################
+
 DINOSAUR_CONTROL_DONE:
     jalr x0, ra, 0
 
 
 ################################################################################
-# Obstacles 
+# OBSTACLES
+#
+# Go through the obstacle array and
+# render the tiles according to the array value.
 ################################################################################
-## Designate a register to be the pointer of the obstacle array
-## Create a pointer for the obstacle position array
-## Create an array that will hold all of the column position values of the obstacles
-## When a value in the obstacle position array reaches 0, change the index of all the values in the array, shifting them forward to the beginning.
-## In other words, delete and shift to save space.
 
 ############################
 # Draw Obstacle
@@ -458,7 +490,9 @@ DRAW_OBSTACLE_LOOP:
     addi t4, t4, 1 # Increment the counter.
     
     slli a1, t4, 2 # Multiply the counter by 4 to make column address value.
-    lw t0, %lo(OBSTACLE_TYPE_ARRAY)(t3) # Get the data from obstacle type array.
+    # Get the data from obstacle type array,
+    # and check which type the obstacle is.
+    lw t0, %lo(OBSTACLE_TYPE_ARRAY)(t3) 
     beq t0, x0, DRAW_BLANK
     li t1, ROCK_VALUE
     beq t0, t1, DRAW_ROCK
@@ -475,16 +509,19 @@ DRAW_ROCK:
     add a1, a1, t1
     sw t0, 0(a1)
 
+    lw t0, %lo(EMPTY_TILE)(gp)                # Load character value to write
     addi t1, a1, NEG_ADDRESSES_PER_ROW
     sw t0, 0(t1)
 
     addi t1, t1, NEG_ADDRESSES_PER_ROW
     sw t0, 0(t1)
 
+    # Check for collision
     li t2, 20
     beq t4, t2, COLLISION_CHECK_ROCK
     li t2, DINOSAUR_COLUMN
     beq t4, t2, COLLISION_CHECK_ROCK
+    
     j DRAWING_DONE
 
 ############################
@@ -502,14 +539,17 @@ DRAW_BAT:
 
     addi t1, a1, NEG_ADDRESSES_PER_ROW
     sw t2, 0(t1)
+
     lw t0, %lo(EMPTY_TILE)(gp)                # Load character value to write
     addi t1, a1, ADDRESSES_PER_ROW
     sw t0, 0(t1)
 
+    # Check for collision
     li t2, 20
     beq t4, t2, COLLISION_CHECK_BAT
     li t2, DINOSAUR_COLUMN
     beq t4, t2, COLLISION_CHECK_BAT
+
     j DRAWING_DONE
 
 ############################
@@ -537,26 +577,143 @@ DRAWING_DONE:
 
 
 ############################
-# Collision check
+# Collision Check
 ############################
 COLLISION_CHECK_ROCK:
     lw t0, %lo(DINOSAUR_STATUS)(gp)
     li t1, DINOSAUR_JUMP_ST
-    beq t0, t1, DRAWING_DONE
-    beq x0, x0, GAMEOVER
+    bne t0, t1, GAMEOVER
+    
+    beq x0, x0, CALCULATE_NEW_SCORE
     
 COLLISION_CHECK_BAT:
     lw t0, %lo(DINOSAUR_STATUS)(gp)
     li t1, DINOSAUR_DUCK_ST
-    beq t0, t1, DRAWING_DONE
-    beq x0, x0, GAMEOVER
+    bne t0, t1, GAMEOVER
 
+############################
+# Calculate New Score
+############################
+CALCULATE_NEW_SCORE:
+    addi sp, sp, -4	    # Make room to save values on the stack
+	sw t3, 0(sp)		# Copy return address to stack
+
+    # Increment score.
+    lw t1, %lo(SCORE)(gp)
+    addi t1, t1, 1
+
+    li t0, 10
+    beq t1, t0, INCREASE_TENS
+
+    addi t2, gp, %lo(SCORE)
+    sw t1, 0(t2)
+
+    j UPDATE_SCORE_ON_DISPLAY
+
+INCREASE_TENS:
+    addi t2, gp, %lo(SCORE)
+    # Set the ones to be 0.
+    sw x0, 0(t2)
+    # Get tens value and increase by one.
+    addi t2, t2, 4
+    lw t1, 0(t2)
+    addi t1, t1, 1
+    # If the tens value is 10, increase hundreds.
+    li t0, 10
+    beq t1, t0, INCREASE_HUNDREDS
+    # If less than 10, update the tens counter.
+    sw t1, 0(t2)
+
+    j UPDATE_SCORE_ON_DISPLAY
+
+INCREASE_HUNDREDS:
+    addi t2, gp, %lo(SCORE)
+    addi t2, t2, 4
+    # Set the tens to be 0.
+    sw x0, 0(t2)
+    # Get hundreds value and increase by one.
+    addi t2, t2, 4
+    lw t1, 0(t2)
+    addi t1, t1, 1
+    # If the hundreds value is 10, increase thousands.
+    li t0, 10
+    beq t1, t0, INCREASE_THOUSANDS
+    # If less than 10, update the hundreds counter.
+    sw t1, 0(t2)
+
+    j UPDATE_SCORE_ON_DISPLAY
+
+INCREASE_THOUSANDS:
+    addi t2, gp, %lo(SCORE)
+    addi t2, t2, 8
+    # Set the tens to be 0.
+    sw x0, 0(t2)
+    # Get hundreds value and increase by one.
+    addi t2, t2, 4
+    lw t1, 0(t2)
+    addi t1, t1, 1
+    # If the hundreds value is 10, increase thousands.
+    li t0, 10
+    beq t1, t0, SCORE_OVER_THE_LIMIT
+    # If less than 10, update the hundreds counter.
+    sw t1, 0(t2)
+
+    j UPDATE_SCORE_ON_DISPLAY
+
+SCORE_OVER_THE_LIMIT:
+    lw t3, 0(sp)		# Restore return address
+	addi sp, sp, 4		# Update stack pointer
+    j GAMEOVER
+
+UPDATE_SCORE_ON_DISPLAY:
+    addi t2, gp, %lo(SCORE)
+    lw t0, 0(t2)
+    lw t1, %lo(FIRST_VAL)(gp)
+    li t3, SCORE_CHAR_BASE
+    add t0, t0, t3
+    sw t0, 0(t1)
+
+    addi t2, t2, 4
+    lw t0, 0(t2)
+    lw t1, %lo(SECOND_VAL)(gp)
+    li t3, SCORE_CHAR_BASE
+    add t0, t0, t3
+    sw t0, 0(t1)
+
+    addi t2, t2, 4
+    lw t0, 0(t2)
+    lw t1, %lo(THIRD_VAL)(gp)
+    li t3, SCORE_CHAR_BASE
+    add t0, t0, t3
+    sw t0, 0(t1)
+
+    addi t2, t2, 4
+    lw t0, 0(t2)
+    lw t1, %lo(FOURTH_VAL)(gp)
+    li t3, SCORE_CHAR_BASE
+    add t0, t0, t3
+    sw t0, 0(t1)
+    
+DONE_CALCULATING_SCORE:
+    lw t3, 0(sp)		# Restore return address
+	addi sp, sp, 4		# Update stack pointer
+    beq x0, x0, DRAWING_DONE
 ############################
 # Game Over
 ############################
 GAMEOVER:
     lw ra, 0(sp)		# Restore return address
 	addi sp, sp, 4		# Update stack pointer
+
+    # Draw Left side
+    lw t0, %lo(DEAD_L)(gp)                   # Load character value to write
+    lw t1, %lo(DINOSAUR_LOC)(gp)             # Load address of character location
+    sw t0, 0(t1)
+
+    # Draw Right side
+    lw t0, %lo(DEAD_R)(gp)                   # Load character value to write
+    addi t1, t1, 4
+    sw t0, 0(t1)
 
 GAMEOVER_WAIT_FOR_NEW_KEY:
     lw t0, BUTTON_OFFSET(tp)
@@ -566,12 +723,26 @@ GAMEOVER_WAIT_FOR_NEW_KEY:
     j RESTART
 
 ################################################################################
-# Data segment
+# DATA SEGMENT
+#
+# This section contains the variables in the memory.
 ################################################################################
 .data
-# Current Dinosaur location.
-DINOSAUR_LOC:
-    .word STARTING_LOC
+# Current Score.
+SCORE:
+    .word 0 0 0 0
+
+FIRST_VAL:
+    .word FIRST_LOC
+
+SECOND_VAL:
+    .word SECOND_LOC
+
+THIRD_VAL:
+    .word THIRD_LOC
+
+FOURTH_VAL:
+    .word FOURTH_LOC
 
 # The location where bats are spawned.
 BAT_SPAWN_LOC:
@@ -588,6 +759,12 @@ DINOSAUR_STATUS:
 # Current Dinosaur run status (to make animation)
 DINOSAUR_RUN_STATUS:
     .word RUN_1_ST
+
+
+
+# Current Dinosaur location.
+DINOSAUR_LOC:
+    .word STARTING_LOC
 
 # Holders for the sprite.
 RUN1_L:
@@ -610,12 +787,16 @@ DUCK2_L:
     .word DINOSAUR_DUCK2_L
 DUCK2_R:
     .word DINOSAUR_DUCK2_R
+DEAD_L:
+    .word DINOSAUR_DEAD_L
+DEAD_R:
+    .word DINOSAUR_DEAD_R
 EMPTY_TILE:
     .word EMPTY_SPACE
 ROCK_SPRITE:
     .word CHAR_ROCK
 BAT_SPRITE:
-    .word CHAR_BAT_U CHAR_BAT_D
+    .word CHAR_BAT_D CHAR_BAT_U
 
 # The index pointer for the obstacle type array.
 OBSTACLE_INDEX_POINTER:
@@ -624,41 +805,44 @@ OBSTACLE_INDEX_POINTER:
 # Use this array to hold the obstacle position.
 # This currently holds 10 obstacles.
 OBSTACLE_TYPE_ARRAY:
-    .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    .word 0 0 0 0 0 0 0 0 2 0 0 0 0 0 0
-    .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    .word 0 0 0 0 0 0 0 0 2 0 0 0 0 0 0
-    .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    .word 0 0 0 0 0 0 0 0 0 0 2 0 0 0 0
-    .word 0 0 0 0 2 0 0 0 0 0 0 0 0 0 0
-    .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    .word 0 0 0 0 2 0 0 0 0 0 0 0 1 0 0
-    .word 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0
-    .word 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0
-    .word 0 0 0 2 0 0 0 0 0 0 0 0 0 0 0
-    .word 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0
-    .word 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0
-    .word 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0
-    .word 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0
-    .word 0 0 0 0 0 0 0 0 0 2 0 0 0 0 0
-    .word 0 0 0 0 1 0 0 0 0 0 0 0 0 2 0
-    .word 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0
-    .word 0 0 2 0 0 0 0 1 0 0 0 0 2 0 0
-    .word 0 1 0 0 0 0 0 1 0 0 0 0 0 0 0
-    .word 0 0 0 0 0 0 2 0 0 0 2 0 0 0 0
-    .word 0 0 0 0 2 0 0 0 0 0 0 0 1 0 0
-    .word 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0
-    .word 0 0 0 0 0 1 0 0 2 0 0 0 0 0 0
-    .word 0 0 1 0 0 0 0 2 0 0 0 0 0 1 0
-    .word 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0
-    .word 0 0 0 0 0 0 0 2 0 0 0 0 0 0 0
-    .word 0 0 0 0 1 0 0 0 2 0 0 0 0 0 0
-    .word 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0
-    .word 0 2 0 0 0 1 0 0 0 0 0 0 0 0 0
-    .word 0 0 0 2 0 2 0 0 0 0 0 1 0 0 2
-######################################################################################
+    .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 # 1
+    .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 # 2
+    .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 # 3
+    .word 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 # 4
+    .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 # 5
+    .word 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 # 6
+    .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 # 7
+    .word 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 # 8
+    .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 # 9
+    .word 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 # 10
+    .word 0 0 0 0 2 0 0 0 0 0 0 0 0 0 0 # 11
+    .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 # 12
+    .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 # 13
+    .word 0 0 0 0 2 0 0 0 0 0 0 0 1 0 0 # 14
+    .word 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 # 15
+    .word 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 # 16
+    .word 0 0 0 2 0 0 0 0 0 0 0 0 0 0 0 # 17
+    .word 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 # 18
+    .word 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 # 19
+    .word 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 # 20
+    .word 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 # 21
+    .word 0 0 0 0 0 0 0 0 0 2 0 0 0 0 0 # 22
+    .word 0 0 0 0 1 0 0 0 0 0 0 0 0 2 0 # 23
+    .word 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 # 24
+    .word 0 0 2 0 0 0 0 1 0 0 0 0 2 0 0 # 25
+    .word 0 1 0 0 0 0 0 1 0 0 0 0 0 0 0 # 26
+    .word 0 0 0 0 0 0 2 0 0 0 2 0 0 0 0 # 27
+    .word 0 0 0 0 2 0 0 0 0 0 0 0 1 0 0 # 28
+    .word 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 # 29
+    .word 0 0 0 0 0 1 0 0 2 0 0 0 0 0 0 # 30
+    .word 0 0 1 0 0 0 0 2 0 0 0 0 0 1 0 # 31
+    .word 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 # 32
+    .word 0 0 0 0 0 0 0 2 0 0 0 0 0 0 0 # 33
+    .word 0 0 0 0 1 0 0 0 2 0 0 0 0 0 0 # 34
+    .word 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 # 35
+    .word 0 2 0 0 0 1 0 0 0 0 0 0 0 0 0 # 36
+    .word 0 0 0 2 0 2 0 0 0 0 0 1 0 0 2 # 37
+    .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 # 38
+    .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 # 39
+    .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 # 40
+################################################################################
